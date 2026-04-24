9c8ea7b7f1c809bc76864da38837184d1db62a70
Toshino <qwixpw@gmail.com>
2026-04-24 20:53:07 +0300

    upd:deb13/wsl2 + nvim 0.13-dev

--- lua/qwixpw/lazy.lua
+++ lua/qwixpw/lazy.lua
@@ -1,4 +1,4 @@
--- Глобальные настройки, которые были в начале packer.lua
+-- Глобальные настройки
 vim.api.nvim_set_var('f', '0fh=i<Tab><Esc>j')
 vim.api.nvim_set_var('q', '02xj')
 vim.api.nvim_set_var('t', '0ea<Tab><Tab><Tab><Esc>j')
@@ -7,19 +7,31 @@ vim.o.termguicolors = true
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Список плагинов
+-- ─────────────────────────────────────────────
+-- Вспомогательная функция (используется zen-mode)
+-- ─────────────────────────────────────────────
+function ColorMyPencils(color)
+    color = color or "tokyonight-moon"
+    vim.cmd.colorscheme(color)
+end
+
+-- ─────────────────────────────────────────────
+-- Плагины
+-- ─────────────────────────────────────────────
 local plugins = {
+
+    -- ── Telescope ──────────────────────────────
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
@@ -28,68 +40,62 @@ local plugins = {
             require('telescope').setup({
                 defaults = {
                     borderchars = {
-                        prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
+                        prompt  = { "─", " ", " ", " ", "─", "─", " ", " " },
                         results = { " " },
                         preview = { " " },
                     },
-                }
+                },
             })
-        end
+
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<C-p>',      builtin.git_files,  {})
+            vim.keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end)
+            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        end,
     },
 
+    -- ── The Matrix colorscheme ─────────────────
     {
         'luisiacc/the-matrix.nvim',
         config = function()
             local colors = require('thematrix.colors').config()
-            vim.g.thematrix_use_original_pallete = 0
-            vim.g.thematrix_transparent_mode = 1
-            vim.g.thematrix_function_style = "NONE"
-            vim.g.thematrix_keyword_style = "italic"
-            vim.g.thematrix_highlights = { Normal = { fg = colors.foreground, bg = "NONE", style = "underline" } }
-            vim.g.thematrix_background_color = {}
-            vim.g.thematrix_telescope_theme = 1
-        end
+            vim.g.thematrix_use_original_pallete  = 0
+            vim.g.thematrix_transparent_mode      = 1
+            vim.g.thematrix_function_style        = "NONE"
+            vim.g.thematrix_keyword_style         = "italic"
+            vim.g.thematrix_highlights            = {
+                Normal = { fg = colors.foreground, bg = "NONE", style = "underline" },
+            }
+            vim.g.thematrix_background_color      = {}
+            vim.g.thematrix_telescope_theme       = 1
+        end,
     },
 
-    -- {
-    --     'jose-elias-alvarez/null-ls.nvim',
-    --     config = function()
-    --         require('null-ls').setup({
-    --             sources = {
-    --                 require('null-ls').builtins.formatting.prettier
-    --             }
-    --         })
-    --     end
-    -- },
-
+    -- ── Rose-pine colorscheme ──────────────────
     {
         'rose-pine/neovim',
         name = 'rose-pine',
         config = function()
-            --vim.cmd('colorscheme rose-pine')
-        end
+            require('rose-pine').setup({
+                disable_background = true,
+            })
+        end,
     },
 
+    -- ── Tokyo Night (активная тема) ────────────
     {
-        "folke/trouble.nvim",
-        opts = {
-            icons = false,
-            -- your configuration comes here
-        }
+        "folke/tokyonight.nvim",
+        lazy     = false,
+        priority = 1000,
+        config   = function()
+            ColorMyPencils()
+        end,
     },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate"
-    },
-
-    "nvim-treesitter/playground",
-    "theprimeagen/harpoon",
-    "theprimeagen/refactoring.nvim",
-    "mbbill/undotree",
-    "tpope/vim-fugitive",
-    "nvim-treesitter/nvim-treesitter-context",
-
+    -- ── Transparent ────────────────────────────
     {
         "xiyaowong/transparent.nvim",
         opts = {
@@ -100,83 +106,267 @@ local plugins = {
                 'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
                 'EndOfBuffer',
             },
-            extra_groups = {},
+            extra_groups   = {},
             exclude_groups = {},
-            on_clear = function() end,
-        }
+            on_clear       = function() end,
+        },
     },
 
+    -- ── Trouble ────────────────────────────────
+    {
+        "folke/trouble.nvim",
+        opts = { icons = false },
+        config = function(_, opts)
+            require('trouble').setup(opts)
+            vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+                { silent = true, noremap = true })
+        end,
+    },
+
+    -- ── Treesitter ─────────────────────────────
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy  = false,
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.config").setup({
+                ensure_installed = { "cpp", "javascript", "typescript", "c", "lua", "rust" },
+                sync_install     = false,
+                auto_install     = true,
+                highlight        = {
+                    enable                            = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end,
+    },
+
+    -- ── Harpoon ────────────────────────────────
+    {
+        "theprimeagen/harpoon",
+        config = function()
+            local mark = require("harpoon.mark")
+            local ui   = require("harpoon.ui")
+
+            vim.keymap.set("n", "<leader>a", mark.add_file)
+            vim.keymap.set("n", "<C-e>",     ui.toggle_quick_menu)
+
+            vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+            vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+            vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+            vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+        end,
+    },
+
+    -- ── Refactoring ────────────────────────────
+    {
+        "theprimeagen/refactoring.nvim",
+        config = function()
+            local ok, refactoring = pcall(require, "refactoring")
+            if not ok then return end
+
+            refactoring.setup({})
+
+            vim.keymap.set("v", "<leader>ri", function()
+                refactoring.refactor('Inline Variable')
+            end, { desc = "Refactor: Inline Variable" })
+        end,
+    },
+
+    -- ── Undotree ───────────────────────────────
+    {
+        "mbbill/undotree",
+        config = function()
+            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+        end,
+    },
+
+    -- ── Fugitive ───────────────────────────────
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+            local group   = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+            local autocmd = vim.api.nvim_create_autocmd
+
+            autocmd("BufWinEnter", {
+                group    = group,
+                pattern  = "*",
+                callback = function()
+                    if vim.bo.ft ~= "fugitive" then return end
+
+                    local bufnr = vim.api.nvim_get_current_buf()
+                    local opts  = { buffer = bufnr, remap = false }
+
+                    vim.keymap.set("n", "<leader>p", function()
+                        vim.cmd.Git('push')
+                    end, opts)
+
+                    vim.keymap.set("n", "<leader>P", function()
+                        vim.cmd.Git({ 'pull', '--rebase' })
+                    end, opts)
+
+                    vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+                end,
+            })
+        end,
+    },
+
+    -- ── Treesitter context ─────────────────────
+    "nvim-treesitter/nvim-treesitter-context",
+
+    -- ── LSP Zero v4 ────────────────────────────
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v4.x',
         dependencies = {
-            -- LSP Support
             { 'neovim/nvim-lspconfig' },
             { 'williamboman/mason.nvim' },
             { 'williamboman/mason-lspconfig.nvim' },
-
-            -- Autocompletion
             { 'hrsh7th/nvim-cmp' },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
             { 'saadparwaiz1/cmp_luasnip' },
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/cmp-nvim-lua' },
-
-            -- Snippets
             { 'L3MON4D3/LuaSnip' },
             { 'rafamadriz/friendly-snippets' },
-        }
-    },
-
-    -- {
-    --     "aserowy/tmux.nvim",
-    --     opts = {
-    --         copy_sync = {
-    --             enable = true,
-    --             ignore_buffers = { empty = false },
-    --             redirect_to_clipboard = false,
-    --             register_offset = 0,
-    --             sync_clipboard = true,
-    --             sync_registers = true,
-    --             sync_registers_keymap_put = true,
-    --             sync_registers_keymap_reg = true,
-    --             sync_deletes = true,
-    --             sync_unnamed = true,
-    --         },
-    --         navigation = {
-    --             cycle_navigation = true,
-    --             enable_default_keybindings = true,
-    --             persist_zoom = false,
-    --         },
-    --         resize = {
-    --             enable_default_keybindings = true,
-    --             resize_step_x = 1,
-    --             resize_step_y = 1,
-    --         }
-    --     }
-    -- },
-
-    "folke/zen-mode.nvim",
-    -- "github/copilot.vim",
-    "eandrju/cellular-automaton.nvim",
-    "laytan/cloak.nvim",
-
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
+        },
         config = function()
-            vim.cmd('colorscheme tokyonight-moon')
-        end
+            local lsp_zero = require('lsp-zero')
+
+            -- Keymaps при подключении LSP
+            lsp_zero.on_attach(function(_, bufnr)
+                local opts = { buffer = bufnr, remap = false }
+
+                vim.keymap.set("n", "gd",          function() vim.lsp.buf.definition()       end, opts)
+                vim.keymap.set("n", "K",            function() vim.lsp.buf.hover()            end, opts)
+                vim.keymap.set("n", "<leader>vws",  function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>vd",   function() vim.diagnostic.open_float()    end, opts)
+                vim.keymap.set("n", "[d",           function() vim.diagnostic.goto_next()     end, opts)
+                vim.keymap.set("n", "]d",           function() vim.diagnostic.goto_prev()     end, opts)
+                vim.keymap.set("n", "<leader>vca",  function() vim.lsp.buf.code_action()      end, opts)
+                vim.keymap.set("n", "<leader>vrr",  function() vim.lsp.buf.references()       end, opts)
+                vim.keymap.set("n", "<leader>vrn",  function() vim.lsp.buf.rename()           end, opts)
+                vim.keymap.set("i", "<C-h>",        function() vim.lsp.buf.signature_help()   end, opts)
+            end)
+
+            -- Иконки диагностики
+            lsp_zero.ui({
+                float_border = 'rounded',
+                sign_icons   = { error = 'E', warn = 'W', hint = 'H', info = 'I' },
+            })
+
+            -- Mason + автонастройка серверов
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                handlers = {
+                    function(server_name)
+                        require('lspconfig')[server_name].setup({})
+                    end,
+                    ['lua_ls'] = function()
+                        require('lspconfig').lua_ls.setup({
+                            settings = {
+                                Lua = {
+                                    diagnostics = { globals = { 'vim' } },
+                                },
+                            },
+                        })
+                    end,
+                },
+            })
+
+            -- nvim-cmp
+            local cmp        = require('cmp')
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+            cmp.setup({
+                sources = {
+                    { name = 'path' },
+                    { name = 'nvim_lsp' },
+                    { name = 'nvim_lua' },
+                    { name = 'luasnip', keyword_length = 2 },
+                    { name = 'buffer',  keyword_length = 3 },
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>']     = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>']     = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>']     = cmp.mapping.confirm({ select = true }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                }),
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+            })
+
+            -- Глобальная конфигурация диагностики
+            vim.diagnostic.config({ virtual_text = true })
+        end,
     },
 
+    -- ── Zen Mode ───────────────────────────────
+    {
+        "folke/zen-mode.nvim",
+        config = function()
+            -- Вариант с числами и rnu
+            vim.keymap.set("n", "<leader>zz", function()
+                require("zen-mode").setup({
+                    window = { width = 90, options = {} },
+                })
+                require("zen-mode").toggle()
+                vim.wo.wrap   = false
+                vim.wo.number = true
+                vim.wo.rnu    = true
+                ColorMyPencils()
+            end)
+
+            -- Вариант без номеров строк
+            vim.keymap.set("n", "<leader>zZ", function()
+                require("zen-mode").setup({
+                    window = { width = 80, options = {} },
+                })
+                require("zen-mode").toggle()
+                vim.wo.wrap          = false
+                vim.wo.number        = false
+                vim.wo.rnu           = false
+                vim.opt.colorcolumn  = "0"
+                ColorMyPencils()
+            end)
+        end,
+    },
+
+    -- ── Cloak ──────────────────────────────────
+    {
+        "laytan/cloak.nvim",
+        config = function()
+            require("cloak").setup({
+                enabled          = true,
+                cloak_character  = "*",
+                highlight_group  = "Comment",
+                patterns         = {
+                    {
+                        file_pattern  = { ".env*", "wrangler.toml", ".dev.vars" },
+                        cloak_pattern = "=.+",
+                    },
+                },
+            })
+        end,
+    },
+
+    -- ── Codeium ────────────────────────────────
     {
         'Exafunction/codeium.vim',
-        config = function()
-        end
-    }
+        config = function() end,
+    },
+
+    -- ── Прочие ─────────────────────────────────
+    "eandrju/cellular-automaton.nvim",
 }
 
+-- ─────────────────────────────────────────────
 -- Инициализация Lazy
+-- ─────────────────────────────────────────────
 require("lazy").setup(plugins)
